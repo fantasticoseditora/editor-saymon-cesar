c7b115e97e4c07d5b8ebeee812707b738347f361
fantasticoseditora <fantasticoseditora@gmail.com>
2026-08-04 22:47:00 -0300

    Configura integração do formulário com o Google Drive

---
+++ form-config.js
@@ -0,0 +1,6 @@
+// Integração do formulário editorial com o Google Apps Script.
+window.EDITORIAL_FORM_CONFIG = Object.freeze({
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbz5gOy4fxhf-_FEfZAo7OMms_fE6ZGs1OmGEdB5bPAWCZCZY29rvmqHeYL0cn2U29uR/exec',
+  maxFileSizeMb: 10,
+  acceptedExtensions: ['pdf', 'doc', 'docx', 'odt', 'rtf']
+});
